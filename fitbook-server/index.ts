a120dec163c0b2264e1f67606263dd521c2d91bd
Hildong <philip.hilding@hotmail.com>
2024-01-08 21:34:31 +0100

    added new scripts

--- fitbook-server/index.ts
+++ fitbook-server/index.ts
@@ -6,7 +6,7 @@ import cors from 'cors'
 import session from 'express-session'
 dotenv.config({path: "./config.env"});
 import passport from 'passport';
-import authRouter from './auth/routes/auth.routes';
+import * as authRouter from './auth/routes/auth.routes';
 import workoutStatsRouter from './routes/workoutStats.route';
 
 const allowedOrigins = ["http://localhost:3000", "https://api.api-ninjas.com", "http://192.168.1.202:3000", "https://ec2-51-20-7-43.eu-north-1.compute.amazonaws.com:3000/", "https://ec2-51-20-7-43.eu-north-1.compute.amazonaws.com:8000/"]
@@ -44,7 +44,7 @@ app.use(passport.session())
 import './auth/passport'
 import { checkAuthenticated } from './auth/auth';
 
-app.use("/auth", authRouter)
+app.use("/auth", authRouter.default)
 app.use("/workout", checkAuthenticated, workoutStatsRouter)
 
 app.get('/', (req: Request, res: Response) => {
