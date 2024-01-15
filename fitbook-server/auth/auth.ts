80e143efbf976b8ecc95c6c640fdeb9de6ffa8df
Hildong <philip.hilding@hotmail.com>
2024-01-15 21:04:23 +0100

    Changed build script

--- fitbook-server/auth/auth.ts
+++ fitbook-server/auth/auth.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-const checkAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+export const checkAuthenticated = (req: Request, res: Response, next: NextFunction) => {
     if(req.isAuthenticated()) return next()
     res.status(401).send("Not allowed")
 }
-
-export default checkAuthenticated
