a9413eff4e6099316386cbb41e642e6e5b2d4be0
Hildong <philip.hilding@hotmail.com>
2024-01-19 13:26:37 +0100

    Updates

--- fitbook-server/auth/auth.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export const checkAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-    if(req.isAuthenticated()) return next()
-    res.status(401).send("Not allowed")
-}
